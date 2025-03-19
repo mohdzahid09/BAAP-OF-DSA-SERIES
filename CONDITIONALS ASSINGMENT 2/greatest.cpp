652d64dcc15e86ea058f0f28b5a93268e34ea7d5
Shahid Icarus <muhammadzzahid0909@gmail.com>
2025-03-19 19:30:14 +0530

    Added solution for [Assignment Name]

---
+++ CONDITIONALS ASSINGMENT 2/greatest.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter number a: ";
+    cin>>a;
+    cout<<"Enter number b: ";
+    cin>>b;
+    if (a>b)
+    {
+        cout<<a<<" is greater than "<<b;
+
+    }
+    else{
+        cout<<b<<" is greater than "<<a;
+    }
+    
+}
